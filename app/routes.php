a904d467bc5e21afe7606ca5c7dadaa3af9b91dd
Taufan Aditya <toopay@taufanaditya.com>
2013-03-02 00:36:27 +0700

    Clean up and fix asset subfolder routes

--- app/routes.php
+++ app/routes.php
@@ -38,7 +38,7 @@ $routes->add('travis_sync', new Route('/synchronize', array(Route::HANDLER => ar
 
 $routes->add('asset', new Route('/{controller}/{action}/{id}', array('controller' => 'asset', 'action' => '(js|css|img|font)', 'id' => '[a-zA-Z\.\-]+')));
 
-$routes->add('asset_subfolder', new Route('/{controller}/{action}/{subfolder}/{id}', array('controller' => 'asset', 'action' => '(js|css|img|font)', 'subfolder' => '[0-9a-zA-Z\_\-]+','id' => '[a-zA-Z\.\-]+')));
+$routes->add('asset_subfolder', new Route('/{controller}/{action}/{subfolder}/{id}', array('controller' => 'asset', 'action' => '(js|css|img|font)', 'subfolder' => '[0-9a-zA-Z\_\-]+','id' => '[0-9a-zA-Z\.\-]+')));
 
 $routes->add('universal_route_default', new Route('/{controller}', array('controller' => '[a-z]+')));
 
