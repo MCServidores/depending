c4a31d21e5e1fbb017296b4c6004dfea6807971d
Taufan Aditya <toopay@taufanaditya.com>
2013-03-06 00:06:46 +0700

    Import repos and enable/disable hook functionalities

--- app/Controller/ControllerHome.php
+++ app/Controller/ControllerHome.php
@@ -8,7 +8,9 @@
 
 namespace app\Controller;
 
+use app\Parameter;
 use app\Model\ModelBase;
+use app\Model\ModelTemplate;
 
 /**
  * ControllerHome
@@ -28,11 +30,66 @@ class ControllerHome extends ControllerBase
 		}
 		// @codeCoverageIgnoreEnd
 
+		// If user already login, find out his repositories
+		$repos = new Parameter(array());
+		if ($this->session->get('login')) {
+			$user = ModelBase::factory('User')->getQuery()->findPK($this->data->get('user')->get('Uid'));
+			$filter = array(
+				array('column' => 'Users', 'value' => $user),
+			);
+
+			$repos = ModelBase::factory('Repo')->getAllRepo(50,1,$filter);
+		}
+
 		// Template configuration
 		$this->layout = 'modules/home/index.tpl';
-		$data = ModelBase::factory('Template')->getHomeData();
+		$data = ModelBase::factory('Template')->getHomeData(compact('repos'));
 
 		// Render
 		return $this->render($data);
+	}
+
+	/**
+	 * Handler untuk GET/POST /home/import
+	 */
+	public function actionImport() {
+		// For AJAX call only
+		if ( ! $this->request->isXmlHttpRequest()) throw new \RangeException('You seems trying to access a different side of this app. Please stop.');
+
+		// @codeCoverageIgnoreStart
+
+		// Initialize result
+		$success = false;
+		$html = '';
+		$repos = array();
+
+		if ($this->session->get('login')) {
+			$uid = $this->session->get('userId');
+			$accessToken = $this->getToken();
+
+			// Check his repository
+			$repositories = ModelBase::factory('Github', new Parameter(array(
+				'githubToken' => $accessToken,
+				)))->getRepositories();
+
+			// Update the repositories data
+			if ($repositories instanceof Parameter) {
+				ModelBase::factory('Repo')->updateUserRepositories($uid,$accessToken,$repositories);
+			}
+
+			if (!empty($repositories)) {
+				$user = ModelBase::factory('User')->getQuery()->findPK($this->session->get('userId'));
+				$repos = $user->getReposs();
+			}
+
+			$success = ! empty($repos);
+			$html = ModelTemplate::render('blocks/list/repo.tpl',compact('repos'));
+		}
+
+		// Just for the sake of jQuery ajax delay
+		sleep(1);
+
+		return $this->renderJson(compact('success','html'));
+		// @codeCoverageIgnoreEnd
 	}
 }
--- app/Controller/ControllerSetting.php
+++ app/Controller/ControllerSetting.php
@@ -8,6 +8,7 @@
 
 namespace app\Controller;
 
+use app\Parameter;
 use app\Model\ModelBase;
 
 /**
@@ -104,5 +105,81 @@ class ControllerSetting extends ControllerBase
 
 		// Render
 		return $this->render($data);
+	}
+
+	/**
+	 * Handler for AJAX POST to enable a repo hook
+	 */
+	public function actionEnable() {
+		// For AJAX call only
+		if ( ! $this->request->isXmlHttpRequest()) {
+			throw new \RangeException('You seems trying to access a different side of this app. Please stop.');
+		}
+
+		// Just for the sake of jQuery ajax delay
+		sleep(1);
+
+		// Initialize result
+		$success = false;
+		$postData = new Parameter($this->data->get('postData'));
+
+		if ($postData->get('repo')) {
+			$uid = $this->session->get('userId');
+			$accessToken = $this->getToken();
+
+			// Enable repository hook
+			$hookSetup = ModelBase::factory('Github', new Parameter(array(
+				'githubToken' => $accessToken,
+				)))->setHookData($postData->get('repo'));
+
+			// Update the repo status data, if success
+			if ($hookSetup) {
+				$activeRepo = ModelBase::factory('Repo')->getQuery()->findOneByFullName($postData->get('repo'));
+				$activeRepo->setStatus(1);
+				$activeRepo->save();
+			}
+
+			$success = $hookSetup;
+		}
+
+		return $this->renderJson(compact('success'));
+	}
+
+	/**
+	 * Handler for AJAX POST to enable a repo hook
+	 */
+	public function actionDisable() {
+		// For AJAX call only
+		if ( ! $this->request->isXmlHttpRequest()) {
+			throw new \RangeException('You seems trying to access a different side of this app. Please stop.');
+		}
+
+		// Just for the sake of jQuery ajax delay
+		sleep(1);
+
+		// Initialize result
+		$success = false;
+		$postData = new Parameter($this->data->get('postData'));
+
+		if ($postData->get('repo')) {
+			$uid = $this->session->get('userId');
+			$accessToken = $this->getToken();
+
+			// Enable repository hook
+			$hookSetup = ModelBase::factory('Github', new Parameter(array(
+				'githubToken' => $accessToken,
+				)))->removeHookData($postData->get('repo'));
+
+			// Update the repo status data, if success
+			if ($hookSetup) {
+				$activeRepo = ModelBase::factory('Repo')->getQuery()->findOneByFullName($postData->get('repo'));
+				$activeRepo->setStatus(0);
+				$activeRepo->save();
+			}
+
+			$success = $hookSetup;
+		}
+
+		return $this->renderJson(compact('success'));
 	}
 }
