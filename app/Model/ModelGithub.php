dada5a354052c008ec43fff6eae255580e863980
Taufan Aditya <toopay@taufanaditya.com>
2013-03-06 00:06:01 +0700

    Add Hook accessors

--- app/Model/ModelGithub.php
+++ app/Model/ModelGithub.php
@@ -59,6 +59,127 @@ class ModelGithub extends ModelBase
     }
 
     /**
+     * Get repository data
+     */
+    public function getRepositories() {
+        if (empty($this->accessToken)) return false;
+
+        $response = $this->getData(self::API_URL.'user/repos', array('access_token' => $this->accessToken));
+
+        if ($response->get('result') == false || strpos($response->get('body'),'full_name')===false) {
+            return false;
+        }
+
+        // Parsing the body
+        if (($repos = json_decode($response->get('body'))) && empty($repos)) {
+            return false;
+        }
+
+        $repos = (array) $repos;
+        $reposData = new Parameter($repos);
+
+        return $reposData;
+    }
+
+    /**
+     * Set Hook data
+     * 
+     * @param string Repo full name
+     * @return bool
+     */
+    public function setHookData($repo) {
+        if (empty($this->accessToken)) return false;
+
+        $hook = array(
+            'name' => 'web',
+            'active' => true,
+            'events' => array('push','pull_request'),
+            'config' => array(
+                'url' => 'http://depending.in/hook',
+                'content_type' => 'json',
+                'secret' => $this->clientSecret,
+            )
+        );
+
+        $response = $this->postJsonData(self::API_URL.'repos/'.$repo.'/hooks?access_token='.$this->accessToken, $hook);
+
+        if (strpos($response->get('result'),'Not Found') !== false || $response->get('head[http_code]',500,true) !== 201) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get Hook data
+     * 
+     * @param string Repo full name
+     * @return Parameter
+     */
+    public function getHookData($repo) {
+        if (empty($this->accessToken)) return false;
+
+        $response = $this->getData(self::API_URL.'repos/'.$repo.'/hooks', array('access_token' => $this->accessToken));
+
+        if ($response->get('result') == false || strpos($response->get('body'),'Not Found')!==false) {
+            return false;
+        }
+
+        // Parsing the body
+        if (($hooks = json_decode($response->get('body'))) && empty($hooks)) {
+            return false;
+        }
+
+        $success = false;
+        $hooks = (array) $hooks;
+
+        foreach ($hooks as $hook) {
+            if ($hook->name == 'web' && strpos($hook->config->url, 'depending.in') !== false) {
+                $success = true;
+                break;
+            }
+        }
+
+        return $success;
+    }
+
+     /**
+     * Remove Hook data
+     * 
+     * @param string Repo full name
+     * @return bool
+     */
+    public function removeHookData($repo) {
+        if (empty($this->accessToken)) return false;
+
+       $response = $this->getData(self::API_URL.'repos/'.$repo.'/hooks', array('access_token' => $this->accessToken));
+
+        if ($response->get('result') == false || strpos($response->get('body'),'Not Found')!==false) {
+            return false;
+        }
+
+        // Parsing the body
+        if (($hooks = json_decode($response->get('body'))) && empty($hooks)) {
+            return false;
+        }
+
+        $success = false;
+        $hooks = (array) $hooks;
+
+        foreach ($hooks as $hook) {
+            if ($hook->name == 'web' && strpos($hook->config->url, 'depending.in') !== false) {
+
+                $deleteResponse = $this->removeData(self::API_URL.'repos/'.$repo.'/hooks/'.$hook->id.'?access_token='.$this->accessToken);
+
+                $success = ($deleteResponse == 204);
+                break;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
      * Retrieve access parameter from code
      * @param string Code
      */
@@ -95,6 +216,56 @@ class ModelGithub extends ModelBase
         return self::AUTHORIZE_URL.'?'.http_build_query($params);
     }
 
+     /**
+     * POST an API JSON data
+     *
+     * @param string $url
+     * @param array $data
+     * @return Parameter
+     * @throws RuntimeException
+     */
+    public function postJsonData($url, $data) {
+         try {
+            // Start output buffer
+            ob_start();
+
+            //open connection
+            $request = curl_init();
+
+            curl_setopt($request, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($request, CURLOPT_URL, $url);
+            curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($request, CURLOPT_FOLLOWLOCATION, TRUE);
+            curl_setopt($request, CURLOPT_VERBOSE, TRUE);
+            curl_setopt($request, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($request, CURLOPT_POST, 1); 
+
+            //execute post
+            $result  = curl_exec($request);
+            $err     = curl_errno($request); 
+            $errmsg  = curl_error($request) ;
+            $head    = curl_getinfo($request);
+
+            //close connection
+            curl_close($request);
+
+            // Capture the buffer and assign into content holder
+            $body = ob_get_clean();
+        } catch (\Exception $e) {
+            throw new \RuntimeException('cURL POST error');
+        }
+
+        return new Parameter(compact('result', 'err', 'errmsg', 'head', 'body'));
+    }
+
+    /**
+     * POST an API data
+     *
+     * @param string $url
+     * @param array $data
+     * @return Parameter
+     * @throws RuntimeException
+     */
     public function postData($url, $data) {
          try {
             // Start output buffer
@@ -124,6 +295,14 @@ class ModelGithub extends ModelBase
         return new Parameter(compact('result', 'err', 'errmsg', 'head', 'body'));
     }
 
+    /**
+     * GET an API data
+     *
+     * @param string $url
+     * @param array $data
+     * @return Parameter
+     * @throws Exception
+     */
     public function getData($url, $data) {
         $url .= '?'.http_build_query($data);
 
@@ -153,5 +332,41 @@ class ModelGithub extends ModelBase
 
         return new Parameter(compact('result', 'err', 'errmsg', 'head', 'body'));
     }
+
+     /**
+     * DELETE an API data
+     *
+     * @param string $url
+     * @return int HTTP Code
+     * @throws Exception
+     */
+    public function removeData($url) {
+        try {
+            // Start output buffer
+            ob_start();
+
+            //open connection
+            $request = curl_init();
+
+            curl_setopt($request, CURLOPT_URL, $url);
+
+            curl_setopt($request, CURLOPT_URL,$url);
+            curl_setopt($request, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+            $result = curl_exec($request);
+            $httpCode = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+            //close connection
+            curl_close($request);
+
+            // Capture the buffer and assign into content holder
+            $body = ob_get_clean();
+        } catch (\Exception $e) {
+            throw new \RuntimeException('cURL DELETE error');
+        }
+
+        return $httpCode;
+    }
     
+   
 }
